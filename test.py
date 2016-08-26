75bd8e734059cbcef511d3b363fa69a9186444c2
tony0325 <tony0325@gmail.com>
2016-08-26 20:01:29 +0800

    Update test.py
    Change the display wording description

--- test.py
+++ test.py
@@ -10,10 +10,10 @@ while 1:
     val2 = adc2.read()/4095*3.3*50*val1
     val1_str = str('%1.2f'%val1)
     val2_str = str('%1.2f'%val2)
-    lcd.write('adc1:')
+    lcd.write('Voltage:')
     lcd.write(val1_str)
     lcd.write('\n')
-    lcd.write('adc2:')
+    lcd.write('Watt:')
     lcd.write(val2_str)
     lcd.write('\n\n\n')
     pyb.delay(1000)
